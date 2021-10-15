53b408fde438ba3d2102c83c20e95cb634c2a026
gischellatte <40295535+gischellatte@users.noreply.github.com>
2021-10-16 00:30:00 +1100

    Create app.component.ts

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+selector: 'app-root',/*used to located custom HTML element and apply component to*/
+template: '<h1>My App</h1>'
+})
+export class AppComponent {
+
+} 

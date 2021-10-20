cebf6cc5a98d52b8f7ade0dfc4f0f50475726d60
gischellatte <40295535+gischellatte@users.noreply.github.com>
2021-10-21 00:41:13 +1100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mw-app',
   template: '<h1>My App</h1>'
+             <p>Keeping track of the media I want to watch</p>
 })
 export class AppComponent {}

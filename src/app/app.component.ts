e7654930f3d73868071c587480dec2fb5fe8c123
gischellatte <40295535+gischellatte@users.noreply.github.com>
2021-10-21 00:52:58 +1100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,5 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selectorurl: 'mw-app',
   template: './app.component.html'
+  /* This is an inline template. The markup is declared within the component metadata. */
+  /*  Using the template metadata property is an easy way to compose our angular components and 
+  we can use back ticks around the inline template value here, 
+  instead of single quotes, and start moving things to multiple lines for better readability.*/
 })
 export class AppComponent {}

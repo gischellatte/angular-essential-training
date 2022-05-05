0747bdba632481501942ae2568769de0f7241f2d
gischellatte <40295535+gischellatte@users.noreply.github.com>
2022-05-06 01:25:27 +1000

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,5 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mw-app',
   templateUrl: './app.component.html'
+  /*We can add the styles property to the object we are passing to our component decorator*/
+  /*This takes an array so we can use a JS array syntax of the double braces*/
+  /*In the [] we can set up our style content via strings */
+  /*Use '' to write style content on multiple lines; content: CSS*/
+  styles: ['
+  h1{color: #fff}
+  '] ; 
 })
 export class AppComponent {}

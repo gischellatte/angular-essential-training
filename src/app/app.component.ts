793087946a45f773ff5945f4918ed7a876813f8b
gischellatte <40295535+gischellatte@users.noreply.github.com>
2022-05-06 02:01:43 +1000

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
   /*In the [] we can set up our style content via strings */
   /*Use '' to write style content on multiple lines; content: CSS*/
     /*array of strings; supporting multiple style strings*/
+  /* the style URLs property works with string values that point to a file that contains the style content.*/
   styleUrls:
   /* change the property to be style URL*/
   /* if we needed to, we could add multiple CSS files to our component, and then simply add them to the array that we set to the style URLs property.*/
